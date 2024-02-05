081deb7194a49be292151701137642b845120f33
Ritika Nunia <ritika@nunia.com>
2024-02-05 17:52:48 +0530

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,27 +6,31 @@ import Seo from '../components/seo'
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
-
 export const query = graphql`
-  query {
-    allFile {
-      nodes {
-        name
+query MyQuery {
+  allMdx(sort: {frontmatter: {date: DESC}}) {
+    nodes {
+      id
+      excerpt
+      frontmatter {
+        date(formatString: "MMMM D, YYYY")
+        title
       }
     }
   }
+}
 `
 
 export const Head = () => <Seo title="My Blog Posts" />
